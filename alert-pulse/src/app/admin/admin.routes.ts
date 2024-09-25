a383e28f5be4ebbf7acf98f3b2ffc93a3ff5d554
jctorresm1 <jc.torresm1@uniandes.edu.co>
2024-09-24 22:38:33 -0500

    se arreglan dependencias

---
+++ alert-pulse/src/app/admin/admin.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { AdminPageComponent } from './admin-page/admin-page.component';
+
+export const adminRoutes: Routes = [
+  { path: '', component: AdminPageComponent }
+];
