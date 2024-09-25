f7c1673ea01f8b7acbb3d6d6781c2df3bf94f211
jctorresm1 <jc.torresm1@uniandes.edu.co>
2024-09-24 22:27:04 -0500

    se agregan componentes de angular material

---
+++ alert-pulse/src/app/material/material.module.ts
@@ -0,0 +1,93 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table';
+import { MatGridListModule, MatGridTile } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatTabsModule, MatTabLink} from '@angular/material/tabs';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { MatDividerModule } from '@angular/material/divider';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatBadgeModule,    
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatTableModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatSortModule,
+    LayoutModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatStepperModule,
+    MatTabsModule,
+    MatExpansionModule,
+    MatChipsModule,
+    MatSliderModule,
+    MatProgressBarModule,
+    MatGridTile,
+    MatSlideToggleModule,
+    MatCardModule,
+    MatDatepickerModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatBadgeModule,    
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatTableModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatSortModule,
+    LayoutModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatStepperModule,
+    MatTabsModule,
+    MatSnackBarModule,
+    MatExpansionModule,
+    MatChipsModule,
+    MatSliderModule,
+    MatProgressBarModule,
+    MatDividerModule,
+    MatGridTile,
+    MatSlideToggleModule,
+    MatCardModule,
+    MatDatepickerModule,
+  ]
+})
+export class MaterialModule { }
