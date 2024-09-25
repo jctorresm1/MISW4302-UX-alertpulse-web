5b1c7f0a2eacc861b38bcf2c27fa9b39d3c58778
jctorresm1 <jc.torresm1@uniandes.edu.co>
2024-09-24 22:22:13 -0500

    componente app

--- alert-pulse/src/app/app.routes.ts
+++ alert-pulse/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/auth/signup', pathMatch: 'full' },  // Redirect default route to login
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes')
+    .then(m => m.authRoutes)  // Lazy load Auth module
+  },
+  {
+    path: 'reminders',
+    loadChildren: () => import('./reminders/reminders.routes')
+    .then(m => m.remindersRoutes)  // Lazy load Reminders module
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module')
+    .then(m => m.AdminModule)  // Lazy load Admin module
+  },
+  { path: '**', redirectTo: '/auth/login' }  // Wildcard route to handle invalid URLs
+];
